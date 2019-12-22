011022433bced6f2de1be42758fc97a20b789349
jason <jason@beansmile.com>
2019-12-22 21:32:13 +0800

    搭建项目基本骨架 初始化router和store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
+import { Provider } from 'react-redux'
 import { GlobalStyle } from './style'
 import { IconStyle } from './assets/iconfont/iconfont'
+import routes from './routes'
+import store from './store'
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle />
-      <IconStyle />
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle />
+        <IconStyle />
+        { renderRoutes(routes) }
+      </HashRouter>
+    </Provider>
   )
 }
 
