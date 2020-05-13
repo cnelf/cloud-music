a9c63ffc0934a89d626dc18ad9dce44efdb06132
jason <jason@beansmile.com>
2020-05-13 15:30:44 +0800

    推荐页请求优化

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseUrl = 'http://localhost:3000'
+export const baseUrl = 'http://localhost:4000'
 
 const axiosInstance = axios.create({
   baseURL: baseUrl
--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -14,8 +14,12 @@ function Recommend(props) {
   const { getBannerDataDispatch, getRecommendListDataDispatch } = props
 
   useEffect(() => {
-    getBannerDataDispatch()
-    getRecommendListDataDispatch()
+    if (!bannerList.size) {
+      getBannerDataDispatch()
+    }
+    if (!recommendList.size) {
+      getRecommendListDataDispatch()
+    }
     // eslint-disable-next-line
   }, [])
 
